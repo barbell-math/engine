0259ef0409671e857842fa322e61c2dedb5a8cb7
Jack Carmichael <carmichaeljr@outlook.com>
2023-05-13 12:33:28 -0600

    Vet workflow checks

--- model/SlidingWindowStateGenerator_test.go
+++ model/SlidingWindowStateGenerator_test.go
@@ -24,25 +24,25 @@ func invalidCheck(slidingWindowSg SlidingWindowStateGen, err error) (func(t *tes
 }
 func TestNewSlidingWindowStateGenInvalidTimeFrameLimits(t *testing.T){
     invalidCheck(NewSlidingWindowStateGen(
-        dataStruct.Pair[int,int]{1,0},dataStruct.Pair[int,int]{0, 1},1,
+        dataStruct.Pair[int,int]{A: 1,B: 0},dataStruct.Pair[int,int]{A: 0, B: 1},1,
     ))(t);
 }
 func TestNewSlidingWindowStateGenInvalidWindowLimits(t *testing.T){
     invalidCheck(NewSlidingWindowStateGen(
-        dataStruct.Pair[int,int]{0, 1},dataStruct.Pair[int,int]{1, 0},1,
+        dataStruct.Pair[int,int]{A: 0, B: 1},dataStruct.Pair[int,int]{A: 1, B: 0},1,
     ))(t);
 }
 func TestNewSlidingWindowStateGenInvalidWindowSize(t *testing.T){
     invalidCheck(NewSlidingWindowStateGen(
-        dataStruct.Pair[int,int]{0, 1},dataStruct.Pair[int,int]{0, 2},1,
+        dataStruct.Pair[int,int]{A: 0, B: 1},dataStruct.Pair[int,int]{A: 0, B: 2},1,
     ))(t);
     invalidCheck(NewSlidingWindowStateGen(
-        dataStruct.Pair[int,int]{1, 2},dataStruct.Pair[int,int]{0, 2},1,
+        dataStruct.Pair[int,int]{A: 1, B: 2},dataStruct.Pair[int,int]{A: 0, B: 2},1,
     ))(t);
 }
 func TestNewSlidingWindowValid(t *testing.T){
     _,err:=NewSlidingWindowStateGen(
-        dataStruct.Pair[int,int]{0, 1},dataStruct.Pair[int,int]{0, 1},1,
+        dataStruct.Pair[int,int]{A: 0, B: 1},dataStruct.Pair[int,int]{A: 0, B: 1},1,
     );
     test.BasicTest(nil,err,
         "Creating a sliding window resulted in an error when it shouldn't have.",t,
@@ -51,7 +51,7 @@ func TestNewSlidingWindowValid(t *testing.T){
 
 func TestNewSlidingWindowConstrainedThreadAllocation(t *testing.T){
     sw,err:=NewSlidingWindowStateGen(
-        dataStruct.Pair[int,int]{0, 1},dataStruct.Pair[int,int]{0, 1},0,
+        dataStruct.Pair[int,int]{A: 0, B: 1},dataStruct.Pair[int,int]{A: 0, B: 1},0,
     );
     test.BasicTest(nil,err,
         "Creating a sliding window resulted in an error when it shouldn't have.",t,
@@ -77,7 +77,7 @@ func TestGenerateModelState(t *testing.T){
     sw,_:=NewSlidingWindowStateGen(
         //dataStruct.Pair[int,int]{4, 500},dataStruct.Pair[int,int]{5, 10},0,
         //dataStruct.Pair[int,int]{0, 500},dataStruct.Pair[int,int]{0, 1},0,
-        dataStruct.Pair[int,int]{0, 500},dataStruct.Pair[int,int]{0, 10},0,
+        dataStruct.Pair[int,int]{A: 0, B: 500},dataStruct.Pair[int,int]{A: 0, B: 10},0,
     );
     err:=sw.GenerateModelState(&testDB,missingModelStateData{
         ClientID: 1,
